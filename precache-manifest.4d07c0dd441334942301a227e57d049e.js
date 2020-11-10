1556059f15f3fa0d33b92268122ce46ff3ab18f2
ragapushadwa <ragapushadwa@gmail.com>
2020-11-10 21:26:25 +0700

    Updates

---
+++ precache-manifest.4d07c0dd441334942301a227e57d049e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28c3615301102a431f45d874caf55e75",
+    "url": "/reactpokedex/index.html"
+  },
+  {
+    "revision": "3cd43c0f4d3aaf02deba",
+    "url": "/reactpokedex/static/css/2.c7b90ac4.chunk.css"
+  },
+  {
+    "revision": "25586dfd845b8553af2d",
+    "url": "/reactpokedex/static/css/main.a12feb68.chunk.css"
+  },
+  {
+    "revision": "3cd43c0f4d3aaf02deba",
+    "url": "/reactpokedex/static/js/2.a06ee26c.chunk.js"
+  },
+  {
+    "revision": "87984cad984dae3222b2a1d0861003a4",
+    "url": "/reactpokedex/static/js/2.a06ee26c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "25586dfd845b8553af2d",
+    "url": "/reactpokedex/static/js/main.a87c8353.chunk.js"
+  },
+  {
+    "revision": "87d73b83246114c063d4",
+    "url": "/reactpokedex/static/js/runtime-main.94ea4f8a.js"
+  }
+]);
